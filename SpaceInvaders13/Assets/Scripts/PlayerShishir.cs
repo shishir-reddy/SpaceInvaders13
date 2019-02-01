bcbb177b164ae0245ae4cbae8ca231d046d624a1
shishir-reddy <shishir1@uci.edu>
2019-01-31 20:19:58 -0800

    Added player movement and shooting

---
+++ SpaceInvaders13/Assets/Scripts/PlayerShishir.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShishir : MonoBehaviour
+{
+    public GameObject Bullet;
+
+    [SerializeField]
+    private float bulletSpeed = 5f;
+
+    [SerializeField]
+    private float playerSpeed = 5f;
+
+
+    [SerializeField]
+    private Rigidbody2D playerRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        Move();
+    }
+
+    /*Instantiate a bullet 1 unit above the player
+     * Set the bulle tto travel upwards
+     * 
+     */
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(Bullet, this.transform.position + Vector3.up, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = Vector3.up * bulletSpeed;
+    }
+
+    private void Move()
+    {
+        float movement = Input.GetAxisRaw("Horizontal");
+        playerRigidBody.velocity = new Vector2(movement * playerSpeed, playerRigidBody.velocity.y);
+    }
+}
