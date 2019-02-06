487881c11e433e092ed80e6ef60cfd68720eaa32
shishir-reddy <shishir1@uci.edu>
2019-02-05 21:03:42 -0800

    Revert "Revert "Created enemy that bounces off wall and is destroyed by bullet""
    This reverts commit 473a2d879944b429c290fb837dd7c77ec6649e18.

---
+++ SpaceInvaders13/Assets/Scripts/EnemyShishir.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShishir : MonoBehaviour
+{
+    public float speed = 2f;
+
+    private Rigidbody2D enemyRigidBody;
+
+    private void Awake()
+    {
+        enemyRigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody.velocity = new Vector2(-1 * speed, enemyRigidBody.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Wall"))
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Collider2D>().CompareTag("Wall"))
+        {
+            enemyRigidBody.position = new Vector2(enemyRigidBody.position.x, enemyRigidBody.position.y - 1);
+            enemyRigidBody.velocity = new Vector2(-1 * enemyRigidBody.velocity.x, enemyRigidBody.velocity.y);
+        }
+    }
+}
