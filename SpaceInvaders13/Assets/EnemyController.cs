def9a5dad4fa7d94f37cb61c88e88bb59c21fa83
M3RLN <32886755+M3RLN@users.noreply.github.com>
2019-02-05 20:16:11 -0800

    Basic Enemy Movement
    Movement Controller in both Enemy Prefab and the GameManager object in my scene

---
+++ SpaceInvaders13/Assets/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public List<GameObject> EnemyList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            for (int i = 0; i<EnemyList.Count; i++)
+            {
+                GameObject controller = EnemyList[i];
+                controller.GetComponent<EnemyScript>().speed = -controller.GetComponent<EnemyScript>().speed;
+                controller.transform.position = new Vector3(controller.transform.position.x, controller.transform.position.y - 1, 0);
+            }
+        }
+    }
+}
