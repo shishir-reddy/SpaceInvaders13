d4cc57c5bc37f8e722c30b2c23062d9d43e517b4
adurchanek <adurchanek@gmail.com>
2019-02-13 19:33:48 -0800

    Index change
    Changed to 0

--- SpaceInvaders13/Assets/Scripts/MainMenu.cs
+++ SpaceInvaders13/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
-        SceneManager.LoadScene("EnemyTestScene");
+        SceneManager.LoadScene(1);
     }
     
     
